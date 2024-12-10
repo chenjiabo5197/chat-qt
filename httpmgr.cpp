8b41fd2d572c6b7fd48fca0a0cf69f33f482e863
chenjiabo5197 <48276511+chenjiabo5197@users.noreply.github.com>
2024-12-10 03:33:22 +0000

    添加注释

--- httpmgr.cpp
+++ httpmgr.cpp
@@ -12,6 +12,7 @@ HttpMgr::HttpMgr()
 
 void HttpMgr::PostHttpReq(QUrl url, QJsonObject json, ReqId req_id, Modules mod)
 {
+    // 使用QJsonDocument封装JSON对象,将JSON文档转换为字符串表示形式
     QByteArray data = QJsonDocument(json).toJson();
     QNetworkRequest request(url);
     // 设置请求类型
@@ -27,8 +28,12 @@ void HttpMgr::PostHttpReq(QUrl url, QJsonObject json, ReqId req_id, Modules mod)
         if(reply->error() != QNetworkReply::NoError){
             qDebug() << reply->errorString();
             // 发送信号通知完成
+            // 调用emit后跟一个信号名和可选的参数时，Qt会查找所有连接到该信号的槽，并依次调用它们。
+            // 这个过程是自动的，由Qt的元对象系统（Meta-Object System）管理
             emit self->sig_http_finish(req_id, "", ErrorCodes::ERR_NETWORK, mod);
             // 回收reply
+            // 安排对象在其事件循环的下一个迭代中被删除。当调用一个对象的 deleteLater() 方法时，该对象并不会立即被删除；
+            // 相反，Qt 会将这个删除操作排队，等到当前事件处理完成后，在适当的时候（通常是在当前事件循环迭代结束时）才实际执行删除操作。
             reply->deleteLater();
             return;
         }
--- httpmgr.h
+++ httpmgr.h
@@ -8,7 +8,7 @@
 #include <QJsonObject>
 #include <QJsonDocument>
 
-// CRTP 奇异递归
+// CRTP 奇异递归模板，一个类继承自一个以其自身为模板参数的模板类，使得基类能够访问派生类的成员，包括私有和保护成员
 class HttpMgr: public QObject, public Singleton<HttpMgr>, public std::enable_shared_from_this<HttpMgr>
 {
     // 支持信号与槽机制
@@ -20,6 +20,7 @@ private:
     // 声明singleton为友元，目的是在singleton的GetInstance函数中需要new T(HttpMgr)对象，为了让其可以访问到HttpMgr的构造函数，所以加一下友元
     friend class Singleton<HttpMgr>;
     HttpMgr();
+    // 用于发送网络请求并接收网络回复
     QNetworkAccessManager _manager;
 
     // 发送http请求
